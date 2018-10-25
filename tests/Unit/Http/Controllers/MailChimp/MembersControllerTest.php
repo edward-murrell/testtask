fe83f25dc666c8a129e328cf039723b0d6563c5f
Edward Murrell <edward.murrell@codefoundation.com.au>
2018-10-25 20:37:58 +1100

    Create first red test for testing the controller.

---
+++ tests/Unit/Http/Controllers/MailChimp/MembersControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\App\Unit\Http\Controllers\MailChimp;
+
+use App\Http\Controllers\MailChimp\MembersController;
+use Illuminate\Http\Request;
+use Tests\App\TestCases\MailChimp\MemberTestCase;
+
+/**
+ * @covers \App\Http\Controllers\MailChimp\MembersController
+ */
+class MembersControllerTest extends MemberTestCase
+{
+    public function dataProviderForExceptions()
+    {
+        yield [
+            'method' => 'create',
+            'list_id' => 'x',
+            'data' => static::$memberData,
+            'request_exception' => 'post',
+            'expected_code' => 400,
+            'expected_content' => '{"message": "MailChimpList[x] not found"}',
+        ];
+    }
+
+    /**
+     * @param $method
+     * @param $list_id
+     * @param $data
+     * @param $request_exception
+     * @param $expected_code
+     * @param $expected_content
+     *
+     * @dataProvider dataProviderForExceptions
+     */
+    public function testErrors($method, $list_id, $data, $request_exception, $expected_code, $expected_content)
+    {
+        $controller = new MembersController($this->entityManager, $this->mockMailChimpForException($request_exception));
+        $request = new Request($data);
+
+        $response = $controller->$method($request, $list_id);
+        $actual_code = $response->getStatusCode();
+        $actual_content = $response->content();
+
+        self::assertEquals($expected_code, $actual_code);
+        self::assertJsonStringEqualsJsonString($expected_content, $actual_content);
+    }
+}
