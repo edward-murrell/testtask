67b0826f7b70c1378a7bb53585a75c36da129d55
Edward Murrell <edward.murrell@codefoundation.com.au>
2018-10-25 20:58:40 +1100

    Create empty MembersController to stop test crashing.

---
+++ app/Http/Controllers/MailChimp/MembersController.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\MailChimp;
+
+use App\Database\Entities\MailChimp\MailChimpList;
+use App\Http\Controllers\Controller;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Mailchimp\Mailchimp;
+
+class MembersController extends Controller
+{
+    /**
+     * @var \Mailchimp\Mailchimp
+     */
+    private $mailChimp;
+
+    /**
+     * ListsController constructor.
+     *
+     * @param \Doctrine\ORM\EntityManagerInterface $entityManager
+     * @param \Mailchimp\Mailchimp $mailchimp
+     */
+    public function __construct(EntityManagerInterface $entityManager, Mailchimp $mailchimp)
+    {
+        parent::__construct($entityManager);
+
+        $this->mailChimp = $mailchimp;
+    }
+
+    public function create(Request $request): JsonResponse
+    {
+
+    }
+}
